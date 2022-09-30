a661c2e3034c48dccc41445ebd35e716efed2717
Juan Villegas <jvillegas422@gmail.com>
2022-09-30 15:01:08 -0500

    Initial client.js setup

--- images/server/public/client.js
+++ images/server/public/client.js
@@ -1,1 +1,48 @@
 console.log('In JS');
+
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('On ready!');
+
+}
+
+function addItemsHere() {
+    let items = {
+        someItems: 'someItems',
+        description: 'description'
+    };
+    $.ajax({
+        url: '/tonsOfItems',
+        method: 'POST',
+        data: items
+    })
+      .then((response) => {
+        console.log('In POST response', response);
+
+        loadItemsHere();
+      })
+        .catch((err) => {
+            console.log('In POST, something went wrong!', err);
+        });
+}
+
+function loadItemsHere() {
+    $.ajax({
+        url: 'tonsOfItems',
+        method: '/GET'
+    })
+      .then((response) => {
+        console.log('/GET respponse', response);
+
+        renderItems();
+      })
+        .catch((err) => {
+            console.log('In GET, something went wrong!', err);
+        });
+}
+
+function renderItems() {
+    console.log('In render items!');
+
+}
